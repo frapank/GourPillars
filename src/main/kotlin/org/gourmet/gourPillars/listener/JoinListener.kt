19aba0bf1565976438deab0048a3086f5a52532f
gourmet <insogna_@outlook.it>
2025-05-19 23:16:01 +0200

    Cleaned code + database integration

--- src/main/kotlin/org/gourmet/gourPillars/listener/JoinListener.kt
+++ src/main/kotlin/org/gourmet/gourPillars/listener/JoinListener.kt
@@ -3,27 +3,26 @@ package org.gourmet.gourPillars.listener
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.scheduler.BukkitRunnable
 import org.gourmet.gourPillars.GourPillars
-import org.gourmet.gourPillars.data.PlayerData
+import org.gourmet.gourPillars.data.DatabaseManager
 import org.gourmet.gourPillars.managers.LevelBarManager
 
 class JoinListener : Listener{
 
-    val jsonManager = GourPillars.jsonManager
 
     @EventHandler
     fun joinEvent(event: PlayerJoinEvent){
 
         GourPillars.spawnManager.teleportPlayerToSpawn(event.player)
-        GourPillars.lobbyScoreboardManager.setScoreboard(event.player)
 
-        val player = event.player
-        val playerData = PlayerData(player.name, 0, 0, 0, 0, 0,0 ,0)
+        object : BukkitRunnable(){
+            override fun run() {
+                GourPillars.lobbyScoreboardManager.setScoreboard(event.player)
+            }
+        }.runTaskLater(GourPillars.instance, 20 * 1)
 
-        jsonManager.setPlayerDataIfAbsent(player, playerData)
-        jsonManager.savePlayerData()
-
-        LevelBarManager.updateLevelInBar(player)
+        LevelBarManager.updateLevelInBar(event.player)
 
     }
 }
