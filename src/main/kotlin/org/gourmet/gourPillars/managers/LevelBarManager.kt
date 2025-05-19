030e354078f5666c67e8861c8ae44c7ccc468bba
gourmet <insogna_@outlook.it>
2025-05-19 23:15:51 +0200

    Cleaned code + database integration

--- src/main/kotlin/org/gourmet/gourPillars/managers/LevelBarManager.kt
+++ src/main/kotlin/org/gourmet/gourPillars/managers/LevelBarManager.kt
@@ -1,15 +1,30 @@
 package org.gourmet.gourPillars.managers
 
 import org.bukkit.entity.Player
+import org.bukkit.scheduler.BukkitRunnable
 import org.gourmet.gourPillars.GourPillars
-import org.gourmet.gourPillars.data.PlayerData
+import org.gourmet.gourPillars.data.DatabaseManager
+import org.gourmet.gourPillars.other.Logger
 
 object LevelBarManager {
 
-    private val jsonManager = GourPillars.jsonManager
+    private val databaseManager = GourPillars.databaseManager
 
+    /*
+        This function will update
+        the level in the bar
+     */
     fun updateLevelInBar(player: Player){
-        val playerData = jsonManager.getPlayerData(player) ?: PlayerData(player.name, 0 , 0 ,0 ,0 ,0, 0, 0)
-        player.level = playerData.level
+        object : BukkitRunnable(){
+            override fun run() {
+                try {
+                    val playerData = databaseManager.playersData.get(player)
+                    player.level = playerData?.stats?.level!!
+                } catch (e: Exception){
+                    Logger.warning("Error in updateLevelInBar ${e.message}")
+                }
+            }
+        }.runTaskLaterAsynchronously(GourPillars.instance, 20 * 1)
+
     }
 }
