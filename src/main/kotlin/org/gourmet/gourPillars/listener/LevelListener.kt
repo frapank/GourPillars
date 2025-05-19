afbb6150c09fc80a97d7778b6c95c808d0eede55
gourmet <insogna_@outlook.it>
2025-05-19 23:15:45 +0200

    Cleaned code + database integration

--- src/main/kotlin/org/gourmet/gourPillars/listener/LevelListener.kt
+++ src/main/kotlin/org/gourmet/gourPillars/listener/LevelListener.kt
@@ -4,18 +4,27 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerExpChangeEvent
 import org.gourmet.gourPillars.GourPillars
-import org.gourmet.gourPillars.data.PlayerData
+import org.gourmet.gourPillars.data.DatabaseManager
+import org.gourmet.gourPillars.other.Logger
 
 class LevelListener : Listener{
 
-    private val jsonManager = GourPillars.jsonManager
+    private val databaseManager = GourPillars.databaseManager
 
+    /*
+        This will set the xp bar with
+        the actual game level
+     */
     @EventHandler
     fun onXpGive(event: PlayerExpChangeEvent){
 
-        val player = event.player
-        val playerData = jsonManager.getPlayerData(player) ?: PlayerData(player.name, 0 , 0 ,0 ,0 ,0,0,0)
+        try {
+            val player = event.player
+            val playerData = databaseManager.playersData.get(player)
 
-        event.amount = playerData.level
+            event.amount = playerData?.stats?.level!!
+        } catch (e: Exception){
+            Logger.warning("Error in onXpGive ${e.message}")
+        }
     }
 }
