30ae2f4e43b67771ff0d0cc9b55f5d86106e3801
gourmet <insogna_@outlook.it>
2025-05-19 23:15:10 +0200

    Cleaned code + database integration

--- src/main/kotlin/org/gourmet/gourPillars/commands/StatsCMD.kt
+++ src/main/kotlin/org/gourmet/gourPillars/commands/StatsCMD.kt
@@ -1,52 +1,29 @@
 package org.gourmet.gourPillars.commands
 
-import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.entity.Player
 import org.gourmet.gourPillars.GourPillars
-import org.gourmet.gourPillars.managers.LevelBarManager
 import org.gourmet.gourPillars.other.messages.MessageData
 import org.gourmet.gourPillars.other.messages.sendDynamicMessage
-import org.gourmet.gourPillars.other.toMini
 import revxrsal.commands.annotation.Command
-import revxrsal.commands.bukkit.annotation.CommandPermission
 
 object StatsCMD {
 
-    private val jsonManager = GourPillars.jsonManager
+    private val databaseManager = GourPillars.databaseManager
 
     @Command("stats")
     fun statsCommand(player: Player){
-        val playerData = jsonManager.getPlayerData(player)
-        val kills = playerData?.kills ?: "error"
-        val deaths = playerData?.deaths ?: "error"
-        val wins = playerData?.wins ?: "error"
-        val defeats = playerData?.defeats ?: "error"
-        val gamesPlayed = playerData?.gamesPlayed ?: "error"
-        val xp = playerData?.xp ?: "error"
-        val level = playerData?.level ?: "error"
-
-        /*
-        player.sendMessage("<gradient:#c061cb:#3584e4>✦━━━━━ Stats ━━━━━✦</gradient>".toMini())
-        player.sendMessage("")
-        player.sendMessage("<gray>👤 Player<gray>:</gray> <yellow>${player.name}</yellow>".toMini())
-        player.sendMessage("<gray>💀 Deaths<gray>:</gray> <yellow>$deaths</yellow>".toMini())
-        player.sendMessage("<gray>⚔ Defeats<gray>:</gray> <yellow>$defeats</yellow>".toMini())
-        player.sendMessage("<gray>🏹 Kills:</gray> <yellow>$kills</yellow>".toMini())
-        player.sendMessage("<gray>🏆 Wins:</gray> <yellow>$wins</yellow>".toMini())
-        player.sendMessage("<gray>🎮 Games Played:</gray> <yellow>$gamesPlayed</yellow>".toMini())
-        player.sendMessage("<gray>⭐ XP:</gray> <yellow>$xp</yellow>".toMini())
-        player.sendMessage("<gray>⬆ Level:</gray> <yellow>$level</yellow>".toMini())
-        player.sendMessage("")
-        player.sendMessage("<gradient:#c061cb:#3584e4>✦━━━━━━━━━━━━━━━━✦</gradient>".toMini())
-         */
+        val playerData = databaseManager.playersData.get(player)
+        val kills = playerData?.stats?.kills ?: "error"
+        val wins = playerData?.stats?.wins ?: "error"
+        val defeats = playerData?.stats?.defeats ?: "error"
+        val xp = playerData?.stats?.xp ?: "error"
+        val level = playerData?.stats?.level ?: "error"
 
         player.sendDynamicMessage(MessageData.STATS_USER,
             "{player}" to player.name,
-            "{deaths}" to deaths.toString(),
             "{defeats}" to defeats.toString(),
             "{kills}" to kills.toString(),
             "{wins}" to wins.toString(),
-            "{gamesPlayed}" to gamesPlayed.toString(),
             "{xp}" to xp.toString(),
             "{level}" to level.toString()
         )
@@ -54,55 +31,22 @@ object StatsCMD {
 
     @Command("stats <target>")
     fun statsCommand(player: Player, target: Player){
-        val playerData = jsonManager.getPlayerData(target)
-        val kills = playerData?.kills ?: "error"
-        val deaths = playerData?.deaths ?: "error"
-        val wins = playerData?.wins ?: "error"
-        val defeats = playerData?.defeats ?: "error"
-        val gamesPlayed = playerData?.gamesPlayed ?: "error"
-        val xp = playerData?.xp ?: "error"
-        val level = playerData?.level ?: "error"
+        val playerData = databaseManager.playersData.get(player)
+        val kills = playerData?.stats?.kills ?: "error"
+        val wins = playerData?.stats?.wins ?: "error"
+        val defeats = playerData?.stats?.defeats ?: "error"
+        val xp = playerData?.stats?.xp ?: "error"
+        val level = playerData?.stats?.level ?: "error"
 
-        val mm = MiniMessage.miniMessage()
-
-        /*
-        val message = mm.deserialize("""
-        <gradient:#ffcc00:#ff6699>✦━━━━━━━━━━━━━━━━━━━━━━━✦</gradient>
-        <bold><gradient:#00ff99:#00ccff>📊 Statistiche Giocatore 📊</gradient></bold>
-        
-        <yellow>👤 Player</yellow> <gray>»</gray> <green>${target.name}</green>
-        <yellow>💀 Deaths</yellow> <gray>»</gray> <red>$deaths</red>
-        <yellow>⚔ Defeats</yellow> <gray>»</gray> <red>$defeats</red>
-        <yellow>🏹 Kills</yellow> <gray>»</gray> <green>$kills</green>
-        <yellow>🏆 Wins</yellow> <gray>»</gray> <aqua>$wins</aqua>
-        <yellow>🎮 Games Played</yellow> <gray>»</gray> <gold>$gamesPlayed</gold>
-        <yellow>⭐ XP</yellow> <gray>»</gray> <light_purple>$xp</light_purple>
-        <yellow>⬆ Level</yellow> <gray>»</gray> <blue>$level</blue>
-        
-        <gradient:#ff6699:#ffcc00>✦━━━━━━━━━━━━━━━━━━━━━━━✦</gradient>
-        """)
-
-        player.sendMessage(message)
-         */
         player.sendDynamicMessage(MessageData.STATS_TARGET,
             "{player}" to player.name,
-            "{deaths}" to deaths.toString(),
             "{defeats}" to defeats.toString(),
             "{kills}" to kills.toString(),
             "{wins}" to wins.toString(),
-            "{gamesPlayed}" to gamesPlayed.toString(),
             "{xp}" to xp.toString(),
             "{level}" to level.toString()
         )
 
 
     }
-
-    @Command("levelset <level>")
-    @CommandPermission("pillars.admin")
-    fun levelSet(player: Player, level: Int){
-        jsonManager.addXP(player, level)
-        LevelBarManager.updateLevelInBar(player)
-    }
-
 }
