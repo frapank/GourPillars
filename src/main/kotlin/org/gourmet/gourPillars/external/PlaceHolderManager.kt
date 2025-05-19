438e8aa94246ef759f24ea21f3021f79835df65e
gourmet <insogna_@outlook.it>
2025-05-19 23:15:32 +0200

    Cleaned code + database integration

--- src/main/kotlin/org/gourmet/gourPillars/external/PlaceHolderManager.kt
+++ src/main/kotlin/org/gourmet/gourPillars/external/PlaceHolderManager.kt
@@ -21,12 +21,12 @@ class PlaceHolderManager : PlaceholderExpansion() {
         }
 
         val gamePlayer: Player = player as Player
-        val playerData = GourPillars.jsonManager.getPlayerData(gamePlayer)
+        val playerData = GourPillars.databaseManager.playersData.get(player)
         if (params.equals("minplayers", ignoreCase = true)) {
             return arenaManager.getArenaByPlayer(gamePlayer)?.minPlayer.toString() ?: "no-arena"
         }
         if (params.equals("waitingplayers", ignoreCase = true)) {
-            return arenaManager.getArenaByPlayer(gamePlayer)?.waitingPlayer?.size.toString() ?: "no-arena"
+            return arenaManager.getArenaByPlayer(gamePlayer)?.inGamePlayer?.size.toString() ?: "no-arena"
         }
         if (params.equals("arenaname", ignoreCase = true)) {
             return arenaManager.getArenaByPlayer(gamePlayer)?.name.toString() ?: "no arena"
@@ -34,7 +34,7 @@ class PlaceHolderManager : PlaceholderExpansion() {
         if (params.equals("aliveplayers", ignoreCase = true)) {
             return arenaManager.getArenaByPlayer(gamePlayer)?.gameTask?.alivePlayer?.size.toString() ?: "no arena"
         }
-        if (params.equals("time", ignoreCase = true)) {
+       if (params.equals("time", ignoreCase = true)) {
             return arenaManager.getArenaByPlayer(gamePlayer)?.gameTask?.getTimeFormatted() ?: "time error"
         }
         if (params.equals("ingamekills", ignoreCase = true)) {
@@ -46,30 +46,24 @@ class PlaceHolderManager : PlaceholderExpansion() {
         if (params.equals("playersinmatch", ignoreCase = true)) {
             var playersInGame = 0
             arenaManager.onlineArenas.forEach{ (_, arena) ->
-                playersInGame += arena.waitingPlayer.size
+                playersInGame += arena.inGamePlayer.size
             }
             return playersInGame.toString()
         }
-        if (params.equals("deaths", ignoreCase = true)) {
-            return playerData?.deaths.toString() ?: "-1"
-        }
         if (params.equals("kills", ignoreCase = true)) {
-            return playerData?.kills.toString() ?: "-1"
+            return playerData?.stats?.kills.toString() ?: "-1"
         }
         if (params.equals("wins", ignoreCase = true)) {
-            return playerData?.wins.toString() ?: "-1"
+            return playerData?.stats?.wins.toString() ?: "-1"
         }
         if (params.equals("defeats", ignoreCase = true)) {
-            return playerData?.defeats.toString() ?: "-1"
-        }
-        if (params.equals("gamesplayed", ignoreCase = true)) {
-            return playerData?.gamesPlayed.toString() ?: "-1"
+            return playerData?.stats?.defeats.toString() ?: "-1"
         }
         if (params.equals("xp", ignoreCase = true)) {
-            return playerData?.xp.toString() ?: "-1"
+            return playerData?.stats?.xp.toString() ?: "-1"
         }
         if (params.equals("level", ignoreCase = true)) {
-            return playerData?.level.toString() ?: "-1"
+            return playerData?.stats?.level.toString() ?: "-1"
         }
 
         return "no-arena-2"
