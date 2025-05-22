a10308d9f29c8f8f91751bd44c22d2f1609219f2
Xxyuri2005xX <yuri.carniti@gmail.com>
2025-05-22 16:14:16 +0200

    disable interact with paintings

--- src/main/kotlin/org/gourmet/gourPillars/listener/lobby/ItemLobbyEvent.kt
+++ src/main/kotlin/org/gourmet/gourPillars/listener/lobby/ItemLobbyEvent.kt
@@ -1,13 +1,19 @@
 package org.gourmet.gourPillars.listener.lobby
 
+import org.bukkit.Material
 import org.bukkit.NamespacedKey
 import org.bukkit.World
+import org.bukkit.entity.GlowItemFrame
 import org.bukkit.entity.ItemFrame
+import org.bukkit.entity.Painting
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
 import org.bukkit.event.block.BlockDropItemEvent
 import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.hanging.HangingBreakByEntityEvent
+import org.bukkit.event.hanging.HangingPlaceEvent
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.event.player.PlayerInteractEntityEvent
@@ -32,6 +38,32 @@ class ItemLobbyEvent : Listener{
 
         e.isCancelled = true
         e.player.performCommand(command)
+
+        val action = e.action
+        val block = e.clickedBlock ?: return
+
+        val type = block.type
+
+        //incomplete - Pot interaction
+        if (type == Material.FLOWER_POT || type.name.startsWith("POTTED_")) {
+            if(isSpawnWorld(e.player.location.world) && (!BuildCMD.buildSessionPlayers.contains(e.player))) {
+                e.isCancelled = true
+            }
+        }
+
+        if (action == Action.RIGHT_CLICK_BLOCK) {
+            val item = e.item
+            if (block.type == Material.FLOWER_POT && item != null && item.type.name.contains("SAPLING", true) ||
+                item!!.type.name.contains("FLOWER", true) ||
+                item.type.name.contains("MUSHROOM", true) ||
+                item.type.name.contains("FERN", true) ||
+                item.type.name.contains("CACTUS", true) ||
+                item.type.name.contains("BAMBOO", true)) {
+                if(isSpawnWorld(e.player.location.world) && (!BuildCMD.buildSessionPlayers.contains(e.player))) {
+                    e.isCancelled = true
+                }
+            }
+        }
     }
 
     @EventHandler
@@ -73,6 +105,27 @@ class ItemLobbyEvent : Listener{
     fun onItemFrameDamage(e: EntityDamageByEntityEvent) {
         if (e.entity is ItemFrame && e.damager is Player && isSpawnWorld(e.entity.location.world) && (!BuildCMD.buildSessionPlayers.contains(e.damager as Player))) {
             e.isCancelled = true
+        }
+    }
+
+    @EventHandler
+    fun onHangingBreak(event: HangingBreakByEntityEvent) {
+        val entity = event.entity
+        if (entity is ItemFrame || entity is Painting || entity is GlowItemFrame) {
+            if (event.remover is Player) {
+                val player = event.remover as Player
+                if(isSpawnWorld(event.remover.location.world) && !BuildCMD.buildSessionPlayers.contains(player)) {
+                    event.isCancelled = true
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    fun onHangingPlace(event: HangingPlaceEvent) {
+        val entity = event.entity
+        if ((entity is ItemFrame || entity is Painting || entity is GlowItemFrame) && isSpawnWorld(event.player!!.location.world) && !BuildCMD.buildSessionPlayers.contains(event.player)) {
+            event.isCancelled = true
         }
     }
 
