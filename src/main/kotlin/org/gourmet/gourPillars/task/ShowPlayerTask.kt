90c582965c495a1356053415f57266a25c539f3d
gourmet <insogna_@outlook.it>
2025-06-05 11:58:00 +0200

    New tab system

--- src/main/kotlin/org/gourmet/gourPillars/task/ShowPlayerTask.kt
+++ src/main/kotlin/org/gourmet/gourPillars/task/ShowPlayerTask.kt
@@ -3,17 +3,28 @@ package org.gourmet.gourPillars.task
 import org.bukkit.Bukkit
 import org.bukkit.scheduler.BukkitRunnable
 import org.gourmet.gourPillars.GourPillars
+import org.gourmet.gourPillars.managers.game.ArenaManager
 
 class ShowPlayerTask : BukkitRunnable() {
-    override fun run() {
-        for (player in Bukkit.getOnlinePlayers()) {
-            for (other in Bukkit.getOnlinePlayers()) {
-                if (player === other) continue
 
-                if (player.world == other.world) {
-                    player.showPlayer(GourPillars.instance, other)
-                } else {
-                    player.hidePlayer(GourPillars.instance, other)
+    val arenaManager: ArenaManager = GourPillars.arenaManager;
+
+    override fun run() {
+        for (viewer in Bukkit.getOnlinePlayers()) {
+            if (arenaManager.isPlayerInArena(viewer)) {
+                for (other in Bukkit.getOnlinePlayers()) {
+                    if (viewer === other) continue
+
+                    if (viewer.world == other.world) {
+                        viewer.showPlayer(GourPillars.instance, other)
+                    } else {
+                        viewer.hidePlayer(GourPillars.instance, other)
+                    }
+                }
+            } else {
+                for (other in Bukkit.getOnlinePlayers()) {
+                    if (viewer === other) continue
+                    viewer.showPlayer(GourPillars.instance, other)
                 }
             }
         }
