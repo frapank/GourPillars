dc310b65b3579e2f2b04d68cc518ada7f09ff231
gourmet <insogna_@outlook.it>
2025-06-18 17:26:41 +0200

    Database database system implemented

--- src/main/kotlin/org/gourmet/gourPillars/commands/StatsCMD.kt
+++ src/main/kotlin/org/gourmet/gourPillars/commands/StatsCMD.kt
@@ -4,6 +4,7 @@ import org.bukkit.entity.Player
 import org.gourmet.gourPillars.GourPillars
 import org.gourmet.gourPillars.other.messages.MessageData
 import org.gourmet.gourPillars.other.messages.sendDynamicMessage
+import org.gourmet.gourPillars.managers.DatabaseManager.PlayerStats
 import revxrsal.commands.annotation.Command
 import revxrsal.commands.bukkit.annotation.CommandPermission
 
@@ -12,18 +13,19 @@ object StatsCMD {
     private val databaseManager = GourPillars.databaseManager
 
     @Command("stats")
-    fun statsCommand(player: Player){
-        val playerData = databaseManager.playersData.get(player)
-        val kills = playerData?.stats?.kills ?: 0
-        val wins = playerData?.stats?.wins ?: 0
-        val xp = playerData?.stats?.xp ?: 0
-        val level = playerData?.stats?.level ?: 0
-        val winStreak = playerData?.stats?.currentWinStreak ?: 0
-        val bestWinStreak = playerData?.stats?.bestWinStreak ?: 0
-        val gamePlayed = playerData?.stats?.playedGame ?: 0
-        val defeats = gamePlayed - wins
+    fun statsCommand(player: Player) {
+        val stats: PlayerStats? = databaseManager.playersStats[player]
+        val kills = stats?.kills ?: 0
+        val wins = stats?.wins ?: 0
+        val xp = stats?.xp ?: 0
+        val level = stats?.level ?: 0
+        val winStreak = stats?.currentWinStreak ?: 0
+        val bestWinStreak = stats?.bestWinStreak ?: 0
+        val gamesPlayed = stats?.playedGame ?: 0
+        val defeats = gamesPlayed - wins
 
-        player.sendDynamicMessage(MessageData.STATS_USER,
+        player.sendDynamicMessage(
+            MessageData.STATS_USER,
             "{player}" to player.name,
             "{defeats}" to defeats.toString(),
             "{kills}" to kills.toString(),
@@ -32,24 +34,25 @@ object StatsCMD {
             "{level}" to level.toString(),
             "{winStreak}" to winStreak.toString(),
             "{bestWinStreak}" to bestWinStreak.toString(),
-            "{gamesPlayed}" to gamePlayed.toString()
+            "{gamesPlayed}" to gamesPlayed.toString()
         )
     }
 
     @Command("stats <target>")
     @CommandPermission("gpillars.stats.other")
-    fun statsCommand(player: Player, target: Player){
-        val playerData = databaseManager.playersData.get(target)
-        val kills = playerData?.stats?.kills ?: 0
-        val wins = playerData?.stats?.wins ?: 0
-        val xp = playerData?.stats?.xp ?: 0
-        val level = playerData?.stats?.level ?: 0
-        val winStreak = playerData?.stats?.currentWinStreak ?: 0
-        val bestWinStreak = playerData?.stats?.bestWinStreak ?: 0
-        val gamePlayed = playerData?.stats?.playedGame ?: 0
-        val defeats = gamePlayed - wins
+    fun statsCommand(player: Player, target: Player) {
+        val stats: PlayerStats? = databaseManager.playersStats[target]
+        val kills = stats?.kills ?: 0
+        val wins = stats?.wins ?: 0
+        val xp = stats?.xp ?: 0
+        val level = stats?.level ?: 0
+        val winStreak = stats?.currentWinStreak ?: 0
+        val bestWinStreak = stats?.bestWinStreak ?: 0
+        val gamesPlayed = stats?.playedGame ?: 0
+        val defeats = gamesPlayed - wins
 
-        player.sendDynamicMessage(MessageData.STATS_TARGET,
+        player.sendDynamicMessage(
+            MessageData.STATS_TARGET,
             "{player}" to target.name,
             "{defeats}" to defeats.toString(),
             "{kills}" to kills.toString(),
@@ -58,9 +61,7 @@ object StatsCMD {
             "{level}" to level.toString(),
             "{winStreak}" to winStreak.toString(),
             "{bestWinStreak}" to bestWinStreak.toString(),
-            "{gamesPlayed}" to gamePlayed.toString()
+            "{gamesPlayed}" to gamesPlayed.toString()
         )
-
-
     }
 }
